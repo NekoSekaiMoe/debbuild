2a0c8f985f79c09e62cf026a4c6889545a091130
SekaiMoe <153579552+SekaiMoe@users.noreply.github.com>
2024-04-11 20:55:36 +0800

    add packages

--- packages/preppipe-gui/build.mk
+++ packages/preppipe-gui/build.mk
@@ -1,14 +1,14 @@
 #!/bin/make -f
 
 ifneq ("$(wildcard $(LICENSE))","")
-WORKDIR = $(shell pwd)/packages/chdman
+WORKDIR = $(shell pwd)/packages/preppipe-gui
 else
 WORKDIR = $(shell pwd)
 endif
 
 
 build:
-	git clone https://github.com/charlesthobe/chdman --depth=1 $(WORKDIR)/source
+	git clone https://github.com/PrepPipe/preppipe-gui --depth=1 $(WORKDIR)/source
 	cd source && cp ../debian -rv source && dpkg-buildpackage -b -us -uc -d
 
 .PHONY: clean
